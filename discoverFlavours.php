4d50d87f2ed40c9f2106279aa5a3adc074d06454
Ravi <ravipadache00@gmail.com>
2024-12-06 20:00:45 +0530

    Create Discover Flavours Page and Design the Page Create Responsive Css As Well

---
+++ discoverFlavours.php
@@ -0,0 +1,80 @@
+<?php
+include "header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discover Flavours</title>
+    <?php
+        include "links.php";
+    ?>
+</head>
+<body>
+<div class="page-header">
+        <h2>Discover Flavours</h2>
+        <div class="contact-breadcrupms">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Discover Flavours</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="flavours-wrapper">
+        <div class="whatisflavours-wrapper">
+            <span>What is Flavours?</span>
+            <p>Nature is brimming with a wide range of flavours, which has inspired mankind since ancient times. The flavouring substance not only enhances the taste but also tickles the olfactory senses. To define flavouring it is an aromatic compound that gives flavour to other substances, altering the characteristics and changing the gestalt of flavour perception.</p>
+        </div>
+
+        <div class="flavours-prepatation-wrapper">
+            <img src="./includes/images/DiscoverFlavours/flavourpreparation.jpg" alt="Flavours Prepatation Image" id="flavours-prepatation-img">
+
+            <div class="flavours-prepatation-main">
+                <span>Flavour preparation</span>
+                <p>Preparation of flavours involves a rigorous process. Some flavours are prepared from essential oils (flavours of Vanilla pods, nuts and citruses) some are prepared from synthetic chemicals. The sweet fruity flavours are derived by the process of expression and the spicy flavours are derived through extraction.</p><br>
+                <p>Some flavouring is prepared by concocting essential oils with synthetic organic chemicals, or entirely from synthetic chemicals with a mix of alcohol, glycerol, Propylene glycol, alone or in combination as solvents.</p>
+            </div>
+
+        </div>
+
+        <div class="discover-flavours">
+            <span>Discover Flavours</span>
+            <div class="discover-flavour-main-wrapper">
+
+                <div class="discover-flavour-main">
+                    <span>Essential Oils</span>
+                    <img src="./includes/images/DiscoverFlavours/Essential Oils.jpg" alt="Essential Oils Image" id="discover-flavour-main-img">
+                    <p>Essential oils are extracted from plants and then mixed with alcohol to bring it to a desirable strength.</p>
+
+                    <p>Essential oils Almond. Anise, Celery, Cinnamon, Clove, Lemon, Nutmeg, Orange, Rosemary, Savory, Basil, Thyme, Wintergreen.</p>
+
+                    <p>Non-Alcoholic Flavouring extract Essential oils are mixed with glycerol or propylene glycol to form a solution.</p>
+                </div>
+
+                <div class="discover-flavour-main">
+                        <span>Fruit Flavouring</span>
+
+                        <img src="./includes/images/DiscoverFlavours/Fruit Flavouring.jpg" alt="Essential Oils Image" id="discover-flavour-main-img">
+                        
+                        <p>Fresh fruits are expressed, concentrated and distilled to give out the sweet scented flavouring. The finished product is generally has between 18 to 22 percent of alcohol in order to prevent fermentation.</p>
+                </div>
+
+
+                <div class="discover-flavour-main">
+                    <span>Artificial Flavouring</span>
+
+                    <img src="./includes/images/DiscoverFlavours/Artificial Flavouring.jpg" alt="Essential Oils Image" id="discover-flavour-main-img">
+
+                    <p>A bit of chemistry is used to imitate and create artificial flavouring. Synthetic flavouring agents are mixed with alcohol, glycerol or propylene glycol to formulate the essence or flavourings.
+                    Vanilla, lemon, lime, banana, cherry, butterscotch, brandy and rum</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
