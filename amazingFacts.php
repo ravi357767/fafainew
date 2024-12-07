1bfe3ac9ac4bd540e05f45e384779a1534bb8c6f
Ravi <ravipadache00@gmail.com>
2024-12-07 18:17:41 +0530

    Create Amazing Facts Page and Added responsive Code

---
+++ amazingFacts.php
@@ -0,0 +1,52 @@
+<?php
+include "header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazing Facts</title>
+    <?php
+    include "links.php";
+    ?>
+</head>
+<body>
+<div class="page-header">
+        <h2>Amazing Facts</h2>
+        <div class="contact-breadcrupms">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Amazing Facts</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="facts-wrapper">
+
+        <div class="facts-mains">
+            <img src="./includes/images/Amazing Facts/tapputti.jpg" alt="Tapputi Image" id="facts-img">
+            <p>Tapputi, the chemist was the perfume maker in 2nd Millennium BC.</p>
+        </div>
+
+        <div class="facts-mains">
+            <img src="./includes/images/Amazing Facts/Egyptian.jpg" alt="Tapputi Image" id="facts-img">
+            <p>Egyptians believed perfumes were the sweat of gods and were the first to develop the art of Perfume Making in 3000 BC The plaster of the Temple of Minerva in Elis was mixed with milk and saffron. So if you rub your wet finger against the wall you can still smell the saffron Persians invented the distillation process and the philosopher Aricenna was one of the first to apply the principles of chemistry to preserve the volatile aromas of flower by distillation.</p>
+        </div>
+
+        <div class="facts-mains">
+            <img src="./includes/images/Amazing Facts/FrenchDuchess.jpg" alt="Tapputi Image" id="facts-img">
+            <p>In the 17th Century, Perfumed Gloves became famous both as an accessory and as the murder weapon. A French duchess was murdered when the poison was rubbed in her gloves and that slowly absorbed into her skin.</p>
+        </div>
+
+        <div class="facts-mains">
+            <img src="./includes/images/Amazing Facts/Roman.jpg" alt="Tapputi Image" id="facts-img">
+            <p>In the 1st Century BC perfumes came to an exotic use. Romans used them on their pets, particularly dogs and horses. At time they even put perfumes on bird wings and let them fly all over the place inorder to spread the fragrance.</p>
+        </div>
+
+    </div>
+
+</body>
+</html>
