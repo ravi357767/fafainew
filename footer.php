86c37116530ac3dc2eada8c9ba2f82bf118706b1
damini_patwa <165820005+daminipatwa@users.noreply.github.com>
2024-12-09 18:30:44 +0530

    footer , slidebar and css changes

--- footer.php
+++ footer.php
@@ -1,8 +1,45 @@
 <head>
-    <?php
-        include "links.php";
-    ?>
+<?php
+    include "links.php";
+?>
 </head>
 <body>
-    
+<footer class="bg-secondary text-light pt-4 pb-2">
+    <div class="container">
+      <div class="row">
+        <!-- Left Section -->
+        <div class="col-lg-6 mb-4">
+          <img src="./includes/images\logo/cropped-fafai_logo.png" alt="Fragrances Flavours Logo" class="mb-2" style="width: 150px;">
+          <p>
+            In 1949, a group of connoisseurs came together to form a fraternity to promote flavours and fragrances in India.
+          </p>
+          <div class="social-icons">
+            <a href="https://www.instagram.com/fafai.social/" class="text-light me-2"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/fragrances-and-flavours-association-of-india-fafai-791687326" class="text-light me-2"><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="https://www.facebook.com/fafai.social/?ref=br_rs" class="text-light me-2"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://www.youtube.com/channel/UCd9mEUvZsMqW5qMxRrVEoyQ" class="text-light me-2"><i class="fa-brands fa-youtube"></i></a>
+            <a href="https://x.com/i/flow/login?redirect_after_login=%2FFAFAI1948" class="text-light me-2"><i class="fa-brands fa-x-twitter"></i></a>
+          </div>
+        </div>
+
+        <!-- Right Section -->
+        <div class="col-lg-6">
+          <h5 class="text-center">Contact</h5>
+          <ul class="list-unstyled">
+            <li><i class="fa-solid fa-phone"></i> +91 22 35100845</li>
+            <li><i class="fa-regular fa-envelope"></i> mail@fafai.in</li>
+            <li>
+            <i class="fa-solid fa-location-dot"></i>
+              Fragrances & Flavours Association Of India Navinchandra Ranchhoddas Shah Hall, 2-B, Court Chambers, Second Floor, 35, Sir Vithaldas Thackersey Marg, Mumbai - 400 020
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="text-center mt-3">
+        <small>&copy;Copyright 2024 FAFAI Design & Developed by ChoreDigital Communication</small>
+      </div>
+    </div>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
---
+++ slidebar.php
@@ -0,0 +1,51 @@
+<head>
+<?php
+    include "links.php";
+?>
+</head>
+<body>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./includes/images\logo/4c66338d88.jpg" class="d-block w-100" alt="...">
+      <div class="caption">
+        <strong>FAFAI Industry Leaders in Aroma and Naturals</strong><br>
+        <span>Focusing on FAFAI's leadership in the fragrance and flavours industry, with visuals of manufactures,aroma chemicals, and natural products from its members. </span><br>
+        <button type="button" class="btn btn-outline-light">KNOW MORE</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./includes/images\logo/40b952ac17.jpg" class="d-block w-100" alt="...">
+      <div class="caption">
+        <strong>FAFAI 60 Years of Excellence</strong><br>
+        <span>A timeline-style banner highlighting FAFAI's growth over 60 years, featuring milestones from its inception to becoming a national asscoiation with 100+ members</span><br>
+        <button type="button" class="btn btn-outline-light">KNOW MORE</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./includes/images\logo/85fecb1a68.jpg" class="d-block w-100" alt="...">
+      <div class="caption">
+        <strong>FAFAI Unity in Frangrance and Favour Industry</strong><br>
+        <span>Illustrated the spirit of unity and fraternity among traders and dealers that led to the creation of FAFAI, with visuals of the early industry collaborations.</span><br>
+        <button type="button" class="btn btn-outline-light">KNOW MORE</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./includes/images\logo/f4048b6594.jpg" class="d-block w-100" alt="...">
+      <div class="caption">
+        <strong>FAFAI National Association of Perfume and Flavours</strong><br>
+        <span>Depicating the national presence of FAFAI,showcasing its vast network of dealers and manufactures in the fragrance, flavour,and aroma industry across Inida.</span><br>
+        <button type="button" class="btn btn-outline-light">KNOW MORE</button>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</body>
