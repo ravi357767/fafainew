80b9a0d71641263d1a755d3d90376c5fc8e58360
Ravi <ravipadache00@gmail.com>
2024-12-09 16:18:19 +0530

    Created Upcoming Seminar Page

---
+++ upcomingSeminar.php
@@ -0,0 +1,36 @@
+<?php
+include "header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upcoming Seminar</title>
+    <?php
+    include "links.php";
+    ?>
+</head>
+
+<body>
+    
+    <div class="page-header">
+        <h2>Upcoming Seminar</h2>
+        <div class="contact-breadcrupms">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Upcoming Seminar</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="online-events-wrapper">
+        <span>Coming Soon...</span>
+    </div>
+
+</body>
+
+</html>
