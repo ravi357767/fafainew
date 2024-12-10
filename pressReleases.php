4c8cc943c028abc04fd645d3a041c50fb490441e
Ravi <ravipadache00@gmail.com>
2024-12-10 18:58:55 +0530

    create press release and Added 3 gallery and Other Elements on Full pages, Created Gallery 4 Pages

---
+++ pressReleases.php
@@ -0,0 +1,203 @@
+<?php
+include "header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Press Releases</title>
+    <?php
+    include "links.php";
+    ?>
+</head>
+
+<body>
+    <div class="page-header">
+        <h2>Press Releases</h2>
+        <div class="contact-breadcrupms">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Press Releases</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="press-release-wrapper">
+        <span>Indian Fragrances and Flavours Reference List of Ingredients</span>
+        <p>2020</p>
+
+        <div class="press-release-main">
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.thehindubusinessline.com/news/fragrances-and-flavours-association-comes-out-with-a-reference-list-of-over-9000-safe-ingredients/article32435687.ece?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/1-1.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.thehindubusinessline.com/news/fragrances-and-flavours-association-comes-out-with-a-reference-list-of-over-9000-safe-ingredients/article32435687.ece?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>The Hindu Business Line (25th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://newspatrolling.com/fafai-unveils-a-list-of-more-than-9000-ingredients-generally-used-and-considered-safe-for-the-indian-fragrances-and-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/10-1.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://newspatrolling.com/fafai-unveils-a-list-of-more-than-9000-ingredients-generally-used-and-considered-safe-for-the-indian-fragrances-and-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>News Patrolling (25th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://timesofindia.indiatimes.com/city/kolkata/fragrance-makers-body-releases-list-of-safe-ingredients/articleshow/77968411.cms" target="_blank"><img src="./includes/images/Press release/11-2.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://timesofindia.indiatimes.com/city/kolkata/fragrance-makers-body-releases-list-of-safe-ingredients/articleshow/77968411.cms" target="_blank"><span>Times of India (7th September 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.perfumerflavorist.com/home/news/21874609/fafai-fragrances-and-flavours-assoc-of-india-fafai-unveils-more-than-9000-ingredients-in-its-industry?utm_source=newsletter-html&utm_medium=email&utm_campaign=PF+E-Newsletter+11-03-2020&absrc=rdm&fbclid=IwAR0ElC61wc5A41JiUqjWHuds9zF-ZUwcO80UHfLOrcBYiKkfinEdsmkRKTg" target="_blank"><img src="./includes/images/Press release/12 (1).jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.perfumerflavorist.com/home/news/21874609/fafai-fragrances-and-flavours-assoc-of-india-fafai-unveils-more-than-9000-ingredients-in-its-industry?utm_source=newsletter-html&utm_medium=email&utm_campaign=PF+E-Newsletter+11-03-2020&absrc=rdm&fbclid=IwAR0ElC61wc5A41JiUqjWHuds9zF-ZUwcO80UHfLOrcBYiKkfinEdsmkRKTg" target="_blank"><span>Perfumer & Flavorist (3rd November 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://m.dailyhunt.in/news/india/english/news+patrolling-epaper-newspatr/fafai+unveils+a+list+of+more+than+9+000+ingredients+generally+used+and+considered+safe+for+the+indian+fragrances+and+flavours+industry-newsid-n209248276?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/2.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://m.dailyhunt.in/news/india/english/news+patrolling-epaper-newspatr/fafai+unveils+a+list+of+more+than+9+000+ingredients+generally+used+and+considered+safe+for+the+indian+fragrances+and+flavours+industry-newsid-n209248276?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>Dailyhunt (25th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://cosmetech.co.in/fafais-unique-initiative-receives-accolades-from-pm-narendra-modi/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/3.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://cosmetech.co.in/fafais-unique-initiative-receives-accolades-from-pm-narendra-modi/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>Cosmetech (25th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://incensemedia.in/stories/fafai-presents-a-list-of-9000-ingredients-for-fragrances-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/4.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+
+                <a href="https://incensemedia.in/stories/fafai-presents-a-list-of-9000-ingredients-for-fragrances-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>Incense Media (25th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.uniindia.com/fafai-unveils-a-list-of-more-than-9-000-ingredients-generally-used-and-considered-safe-for-indian-fragrances-and-flavours-industry/business-economy/news/2138029.html?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/5.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+
+                <a href="https://www.uniindia.com/fafai-unveils-a-list-of-more-than-9-000-ingredients-generally-used-and-considered-safe-for-indian-fragrances-and-flavours-industry/business-economy/news/2138029.html?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>United News of India (27th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.fnbnews.com/Top-News/fafai-releasees-first-edition-of-indian-fragrances-and-flavours-ref-list-60778?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/6.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+
+                <a href="https://www.fnbnews.com/Top-News/fafai-releasees-first-edition-of-indian-fragrances-and-flavours-ref-list-60778?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>FNB News (28th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.apnnews.com/fafai-unveils-a-list-of-more-than-9000-ingredients-generally-used-and-considered-safe-for-the-indian-fragrances-and-flavours-industry-2/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/7.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+
+                <a href="https://www.apnnews.com/fafai-unveils-a-list-of-more-than-9000-ingredients-generally-used-and-considered-safe-for-the-indian-fragrances-and-flavours-industry-2/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>APN News (27th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.pninews.com/fafai-unveils-more-than-9000-ingredients-used-in-indian-fragrances-and-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/8.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+
+                <a href="https://www.pninews.com/fafai-unveils-more-than-9000-ingredients-used-in-indian-fragrances-and-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>Press Network of India (27th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://unkrate.com/2020/08/fafai-unveiled-a-list-of-9000-ingredients-used-and-considered-safe-for-the-indian-fragrances-and-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><img src="./includes/images/Press release/9.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+
+                <a href="https://unkrate.com/2020/08/fafai-unveiled-a-list-of-9000-ingredients-used-and-considered-safe-for-the-indian-fragrances-and-flavours-industry/?fbclid=IwAR3nECYZm-fWKIETONEdgT9B-v5cbyIrTR9U7prRQmGhCZzfKF_cBLHveD4" target="_blank"><span>Unkrate (25th August 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+        </div>
+        <span>Pandemic and F&F Industry</span>
+        <p>2020</p>
+        <div class="press-release-main">
+
+            <div class="press-release-article-wrapper">
+                <a href="https://perfumerflavorist.texterity.com/perfumerflavorist/july_2020/MobilePagedArticle.action?articleId=1595575&fbclid=IwAR01p08oMRgTyiDnac23fmdCt0hNqoC98m0hvCsTyp9Vrd-8IWc4mlKYNkE#articleId1595575" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n1.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://perfumerflavorist.texterity.com/perfumerflavorist/july_2020/MobilePagedArticle.action?articleId=1595575&fbclid=IwAR01p08oMRgTyiDnac23fmdCt0hNqoC98m0hvCsTyp9Vrd-8IWc4mlKYNkE#articleId1595575" target="_blank"><span>Perfumer & Flavorist (July 2020 Edition)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.perfumerflavorist.com/news/news/21873706/indias-lockdown-halts-ff-ingredient-production?fbclid=IwAR2U5gIpBEIXXKEzEYd2p_O8TGopjKwVI2bakEmoC7lv5n-WHZLLpbKW6uw4" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n10.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.perfumerflavorist.com/news/news/21873706/indias-lockdown-halts-ff-ingredient-production?fbclid=IwAR2U5gIpBEIXXKEzEYd2p_O8TGopjKwVI2bakEmoC7lv5n-WHZLLpbKW6uw4" target="_blank"><span>Perfumer & Flavorist (31st March 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.moneycontrol.com/news/business/losing-sense-of-taste-smell-indian-fragrances-and-flavours-industry-fears-falling-behind-china-5320761.html?fbclid=IwAR2yEtX3-Hdu-Ho-HbG6pkwD-OmvjR_wUaZI_luqu88j7f9-Ojg5CizKCFc" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n2.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.moneycontrol.com/news/business/losing-sense-of-taste-smell-indian-fragrances-and-flavours-industry-fears-falling-behind-china-5320761.html?fbclid=IwAR2yEtX3-Hdu-Ho-HbG6pkwD-OmvjR_wUaZI_luqu88j7f9-Ojg5CizKCFc" target="_blank"><span>Money Control (27th May 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.cosmeticsdesign-asia.com/Article/2020/06/16/Critical-for-growth-Fragrance-and-flavours-R-D-centre-to-open-by-Indian-association-by-end-year/?fbclid=IwAR2YaqFbr5HbM7ruWmieeOXoVLD-2f8ftfJvI_IeQIq788JeZUW8jxYLZgk" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n3.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.cosmeticsdesign-asia.com/Article/2020/06/16/Critical-for-growth-Fragrance-and-flavours-R-D-centre-to-open-by-Indian-association-by-end-year/?fbclid=IwAR2YaqFbr5HbM7ruWmieeOXoVLD-2f8ftfJvI_IeQIq788JeZUW8jxYLZgk" target="_blank"><span>Cosmetics Design Asia (16th June 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.thehindubusinessline.com/companies/amid-lockdown-fragrances-and-flavours-industry-smells-increased-competition-from-china/article31566201.ece?fbclid=IwAR26eyWDLg7Lq1hTHLzuhycit8tluqJZw45skDczx-SlYLyZCPG7tMG9-ig" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n4.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.thehindubusinessline.com/companies/amid-lockdown-fragrances-and-flavours-industry-smells-increased-competition-from-china/article31566201.ece?fbclid=IwAR26eyWDLg7Lq1hTHLzuhycit8tluqJZw45skDczx-SlYLyZCPG7tMG9-ig" target="_blank"><span>The Hindu Business Line (12th May 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.uniindia.com/govt-should-bail-out-fragrances-industry-by-announcing-fiscal-stimulus-package-fafai/business-economy/news/1992849.html?fbclid=IwAR2LmjfdtkPOMe9h4x13uKea7GM7CaI45Fk8K8uY1djjWgUpYSfwTxE8wE8#.XsrLyFKVKSE.facebook" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n5.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.uniindia.com/govt-should-bail-out-fragrances-industry-by-announcing-fiscal-stimulus-package-fafai/business-economy/news/1992849.html?fbclid=IwAR2LmjfdtkPOMe9h4x13uKea7GM7CaI45Fk8K8uY1djjWgUpYSfwTxE8wE8#.XsrLyFKVKSE.facebook" target="_blank"><span>United News of India (12th May 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://www.financialexpress.com/business/industry-lockdown-impact-supply-disruption-hits-fragrance-and-flavour-industry-with-80-production-loss-1956810/" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n6-1.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://www.financialexpress.com/business/industry-lockdown-impact-supply-disruption-hits-fragrance-and-flavour-industry-with-80-production-loss-1956810/" target="_blank"><span>Financial Express (13th May 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://timesofindia.indiatimes.com/business/india-business/indian-fragrances-flavours-industry-fears-losing-ground-to-china-on-exports/articleshow/75697445.cms?utm_source=facebook.com&utm_medium=social&utm_campaign=TOIBusiness&fbclid=IwAR2bTs5ekbf9ovxnybjGkDurbv1lIYnfpjh2fruPaATte46tOLco0e-GT04" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n8.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://timesofindia.indiatimes.com/business/india-business/indian-fragrances-flavours-industry-fears-losing-ground-to-china-on-exports/articleshow/75697445.cms?utm_source=facebook.com&utm_medium=social&utm_campaign=TOIBusiness&fbclid=IwAR2bTs5ekbf9ovxnybjGkDurbv1lIYnfpjh2fruPaATte46tOLco0e-GT04" target="_blank"><span>Times of India (12th May 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+            <div class="press-release-article-wrapper">
+                <a href="https://bartamanpatrika.com/detailNews.php?cID=19&nID=223675&P=1&fbclid=IwAR01p08oMRgTyiDnac23fmdCt0hNqoC98m0hvCsTyp9Vrd-8IWc4mlKYNkE" target="_blank"><img src="./includes/images/Pandemic and F&F Industry/n9.jpg" alt="Press-release-article" id="Press-release-article-img"></a>
+                <a href="https://bartamanpatrika.com/detailNews.php?cID=19&nID=223675&P=1&fbclid=IwAR01p08oMRgTyiDnac23fmdCt0hNqoC98m0hvCsTyp9Vrd-8IWc4mlKYNkE" target="_blank"><span>Bartaman Patrika (17th April 2020)</span></a>
+                <p>Read More...</p>
+            </div>
+
+        </div>
+        <span>FAFAI Aroma Workshop Kanpur</span>
+        <p>17th December 2019 (Press Releases)</p>
+        <div class="press-release-main">
+            <?php
+                include "./includes/gallery/FAFAIaromaworkshopkanpur.php";
+            ?>
+        </div>
+
+        <span>FAFAI Fragrance Workshop Indore</span>
+        <p>22nd September 2019 (Press Releases)</p>
+        <div class="press-release-main">
+            <?php
+                include "./includes/gallery/FAFAIfragranceworkshopindore.php";
+            ?>
+        </div>
+
+        <span>New President Election</span>
+        <p>11th July 2019 (Press Releases)</p>
+        <div class="press-release-main">
+            <?php
+                include "./includes/gallery/newpresidentelection.php";
+            ?>
+        </div>
+
+        
+
+    </div>
+
+</body>
+
+</html>
