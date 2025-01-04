f3cfe42e6ff4e0a1e2e9a641f6ff4b1060c848d9
Ravi <ravipadache00@gmail.com>
2025-01-04 20:26:20 +0530

    Added go to Top button on Every Page of FAFAI

--- links.php
+++ links.php
@@ -11,3 +11,7 @@
 <!-- FontAwesome CDN -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- FontAwesome CDN -->
+
+<div class="go-to-top-wrapper">
+        <a href="#" id="go-to-top"><button><i class="fa-solid fa-up-long"></i></button></a>
+    </div>
